62ea2ef635a49bec7ff682f5203a224b714d8d88
Bradley Booth <bradley.booth@gmail.com>
2014-12-07 12:23:06 -0700

    Add md5 to tree to fix production build

--- Brocfile.js
+++ Brocfile.js
@@ -22,4 +22,6 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/blueimp-md5/js/md5.min.js');
+
 module.exports = app.toTree();
