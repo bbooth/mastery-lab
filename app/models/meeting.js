7de39ff50d2cbe87e51eb16b8efca6587ed28d7d
Bradley Booth <bradley.booth@gmail.com>
2014-12-07 20:46:59 -0700

    Add attendees relationship

--- app/models/meeting.js
+++ app/models/meeting.js
@@ -2,13 +2,14 @@ import DS from 'ember-data';
 
 var Meeting = DS.Model.extend({
     title: DS.attr('string'),
-    duration: DS.attr('string')
+    duration: DS.attr('string'),
+    attendees: DS.hasMany('person', {async: true})
 });
 
 Meeting.reopenClass({
     FIXTURES: [
-        { id: 1, title: 'Mastery Lab - iOS', duration: '2h' },
-        { id: 2, title: 'Mastery Lab - Ember', duration: '30m' }
+        { id: 1, title: 'Mastery Lab - iOS', duration: '2h', attendees: [1, 2, 3, 4, 5] },
+        { id: 2, title: 'Mastery Lab - Ember', duration: '30m', attendees: [4, 5] }
     ]
 });
 
